e3b7143c6d5af73e87db3bbef2b8a6291f3f28d9
gmhcode <greg.hughes@meritagepos.com>
2020-04-18 16:05:36 -0600

    working on hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,48 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
-
-//Goes to the public directory
-const publicDirectoryPath = path.join(__dirname, '../public')
 const app = express()
+//Define paths for express the 'Serve'
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+//Setup Handlebars engine
+app.set('view engine', 'hbs')
+//Tells express to use the templates folder
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
 
 //Tells the server to use the html page
 app.use(express.static(publicDirectoryPath))
 
 
 app.get('', (req, res) => {
-  res.send('<h1>Weather</h1>')
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Greg Hughes'
+  })
 })
 
-// app.get('/help', (req, res) => {
-//   res.send({
-//     name: 'Greg',
-//     age: 31
-//   })
-// })
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'HelpText',
+    title: 'Help',
+    name: 'Greg Hughes'
+  })
+})
 
-// app.get('/about', (req, res) => {
-//   res.send('<h1>About</h1>')
-// })
+app.get('/about', (req, res) => {
+
+  res.render('about', {
+    title: 'About Me',
+    name: 'Greg Hughes'
+  })
+})
+
+
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'forecast',
