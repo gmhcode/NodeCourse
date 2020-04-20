28523cb080eeafb6644e24ba2a486fbe9f6180d3
gmhcode <greg.hughes@meritagepos.com>
2020-04-19 18:50:07 -0600

    built that app node

--- BTANode/app.js
+++ BTANode/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+app.use(morgan('combined'))
 
 app.get("/", (req, res) => {
   console.log("Responding to root route")
@@ -7,6 +10,13 @@ app.get("/", (req, res) => {
 })
 
 
+app.get("/users", (req, res) => {
+  var user1 = { firstName: "Greg", lastName: "Hughes" }
+  const user2 = { firstName: "Kevin", lastName: "Durant" }
+  res.json([user1, user2])
+
+})
+
 app.listen(3003, () => {
-  console.log("Server is up and listening")
+  console.log("Server is up and listening on 3003....")
 })
