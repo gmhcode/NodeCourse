408586744bf262e6c7eda7bcf2c7914febd4deb5
Greg Hughes <dcgg_101@hotmail.com>
2020-04-21 14:22:09 -0600

    refatored and put users in a different file

--- BTANode/app.js
+++ BTANode/app.js
@@ -2,92 +2,29 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const mysql = require('mysql')
-
+const router = require('./routes/user.js')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(express.static('./public'))
-
+app.use(router)
 app.use(morgan('short'))
-
-app.post('/user_create', (req, res) => {
-  console.log("Trying To create a new user")
-  console.log("How do we get the form data")
-
-  const firstName = req.body.create_first_name
-  const lastName = req.body.create_last_name
-  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
-
-  getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
-    if (err) {
-      console.log("Failed to insert new user: " + err)
-      res.sendStatus(500)
-      return
-    }
-
-    console.log("Inserted a new user with id: ", results.insertId)
-    res.end
-  })
-
-})
-
-
-function getConnection() {
-  return mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'lbta_mysql_fixed'
-  })
-}
-
-app.get('/user/:id', (req, res) => {
-  console.log('Fething user with id: ' + req.params.id)
-
-  const connection = getConnection()
-
-  const userID = req.params.id
-  const queryString = "SELECT * FROM users WHERE id = ?"
-
-  connection.query(queryString, [userID], (err, rows, fields) => {
-
-    if (err) {
-      console.log("Failed to query for users: " + err)
-      res.sendStatus(500)
-      return
-    }
-    console.log("I think we fetched users successfully")
-
-    const users = rows.map((row) => {
-      return { firstName: row.first_name, lastName: row.last_name }
-    })
-    res.json(users)
-  })
-})
 
 app.get("/", (req, res) => {
   console.log("Responding to root route")
   res.send("Hello from ROOOOOOT")
 })
 
+
+
+
+
+
+
+
+
 // http://192.168.1.8/
-app.get("/users", (req, res) => {
-  const connection = getConnection()
-  const queryString = "SELECT * FROM users"
-  connection.query(queryString, (err, rows, fields) => {
 
-    if (err) {
-      console.log("Failed to query for users: " + err)
-      res.sendStatus(500)
-      return
-    }
-    console.log("I think we fetched users successfully")
-
-
-    res.json(rows)
-  })
-
-})
 
 // app.listen(3003, '0.0.0.0',() => {
 //   console.log("Server is up and listening on 3003....")
---
+++ BTANode/routes/user.js
@@ -0,0 +1,85 @@
+//Will contain all of my user related routes
+const express = require('express')
+const mysql = require('mysql')
+
+const router = express.Router()
+router.get('/messages', (req, res) => {
+    console.log("Show some messages or whatever...")
+    res.end()
+})
+
+router.get("/users", (req, res) => {
+    const connection = getConnection()
+    const queryString = "SELECT * FROM users"
+    connection.query(queryString, (err, rows, fields) => {
+
+        if (err) {
+            console.log("Failed to query for users: " + err)
+            res.sendStatus(500)
+            return
+        }
+        console.log("I think we fetched users successfully")
+
+
+        res.json(rows)
+    })
+
+})
+
+function getConnection() {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'lbta_mysql_fixed'
+    })
+}
+
+router.post('/user_create', (req, res) => {
+    console.log("Trying To create a new user")
+    console.log("How do we get the form data")
+
+    const firstName = req.body.create_first_name
+    const lastName = req.body.create_last_name
+    const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
+
+    getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
+        if (err) {
+            console.log("Failed to insert new user: " + err)
+            res.sendStatus(500)
+            return
+        }
+
+        console.log("Inserted a new user with id: ", results.insertId)
+        res.end
+    })
+
+})
+
+
+
+
+router.get('/user/:id', (req, res) => {
+    console.log('Fething user with id: ' + req.params.id)
+
+    const connection = getConnection()
+
+    const userID = req.params.id
+    const queryString = "SELECT * FROM users WHERE id = ?"
+
+    connection.query(queryString, [userID], (err, rows, fields) => {
+
+        if (err) {
+            console.log("Failed to query for users: " + err)
+            res.sendStatus(500)
+            return
+        }
+        console.log("I think we fetched users successfully")
+
+        const users = rows.map((row) => {
+            return { firstName: row.first_name, lastName: row.last_name }
+        })
+        res.json(users)
+    })
+})
+
+module.exports = router
